8c3ac2c9fd4e31f517b72cddec38f6452d7d3268
Aldi <aldiaafn16@gmail.com>
2025-05-23 21:32:27 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -129,8 +129,7 @@ const startServer = async () => {
     await initDB();
     console.log("Database synced!");
 
-    const PORT = process.env.PORT || 5000;
-
+    const PORT = 3000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   } catch (error) {
     console.error("Failed to start server:", error);
